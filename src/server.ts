2e06e37cbfd92af924d48f2f80a1ee50022e6329
DoniKing48 <overnelord@gmail.com>
2025-03-11 17:56:58 +0500

    fix: Initial Setup

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-console.log(9800);
-
-import moment from 'moment';
-
-const currntTime = moment().format("YYYY MM DD");
-console.log(currntTime);
-
-const person: string = "Dan";
-const count: number = 18;
-=======
 // architectural pattern: Model View Controller, Dependency Injection, Model View Presenter
 
 // design pattern: Middleware, Decorator
->>>>>>> 098ffeb (Update server.ts)
