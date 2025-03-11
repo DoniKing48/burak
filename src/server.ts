035bcbbddf166e12ef334fd0e051f6818a4da134
DoniKing48 <overnelord@gmail.com>
2025-03-11 17:56:58 +0500

    fix: Initial Setup

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-console.log(9800);
-
-import moment from 'moment';
-
-const currntTime = moment().format("YYYY MM DD");
-console.log(currntTime);
-
-const person: string = "Dan";
-const count: number = 18;
