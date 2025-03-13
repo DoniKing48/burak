904763191ee3bcdee97497dd4bc2816dd364ec25
DoniKing48 <overnelord@gmail.com>
2025-03-13 08:17:57 +0500

    feat: integration of enviromental variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,3 @@
-console.log(9800);
+// architectural pattern: Model View Controller, Dependency Injection, Model View Presenter
 
-import moment from 'moment';
-
-const currntTime = moment().format("YYYY MM DD");
-console.log(currntTime);
-
-const person: string = "Dan";
-const count: number = 18;
+// design pattern: Middleware, Decorator
