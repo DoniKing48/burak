18342c7c8e4d66da59da19c940a2d98fea0f19c3
DoniKing48 <overnelord@gmail.com>
2025-03-13 08:27:37 +0500

    WIP: Save changes before pulling

---
+++ src/train.ts
@@ -0,0 +1,7 @@
+// G-TASK
+
+// function getHighestIndex(arr: number[]): number {
+//     return arr.indexOf(Math.max(...arr));
+// }
+
+// console.log(getHighestIndex([5, 21, 122, 21, 1008]));
--- train.ts
+++
@@ -1,5 +0,0 @@
-function getHighestIndex(arr: number[]): number {
-    return arr.indexOf(Math.max(...arr));
-}
-
-console.log(getHighestIndex([5, 21, 122, 21, 1008]));
