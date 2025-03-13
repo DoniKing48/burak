ce4c38629911a31ae222dac24e7c69cd40a2c857
DoniKing48 <overnelord@gmail.com>
2025-03-13 09:28:37 +0500

    feat: H-TASK

--- src/train.ts
+++ src/train.ts
@@ -5,3 +5,10 @@
 // }
 
 // console.log(getHighestIndex([5, 21, 122, 21, 1008]));
+
+// H-TASK
+
+function positiveNumbers(arr: number[]): number[] {
+    return arr.filter(num => num > 0);
+}
+console.log(positiveNumbers([-1,-2,-3,4,5,6,-7,8]));
