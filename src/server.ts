cb1dd471069dc75a4e739f70d2cb36620adb3256
DoniKing48 <overnelord@gmail.com>
2025-03-13 08:40:56 +0500

    fix: fixed probs

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,2 @@
-// architectural pattern: Model View Controller, Dependency Injection, Model View Presenter
-
-// design pattern: Middleware, Decorator
+import dotenv from "dotenv";
+dotenv.config();
