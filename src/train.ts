4d2a25017e726f60f3b21d4826fa04eefa6ad896
DoniKing48 <overnelord@gmail.com>
2025-03-14 05:59:56 +0500

    fix: H-TASK

--- src/train.ts
+++ src/train.ts
@@ -8,7 +8,7 @@
 
 // H-TASK
 
-function positiveNumbers(arr: number[]): number[] {
-    return arr.filter(num => num > 0);
+function positiveNumbers(arr: number[]): string[] {
+    return arr.filter(str => str > 0).map(String);
 }
 console.log(positiveNumbers([-1,-2,-3,4,5,6,-7,8]));
