bd84baf02714a674a8268873151d3aaf7ce599ea
DoniKing48 <overnelord@gmail.com>
2025-03-16 14:17:24 +0500

    feat: I-TASk

--- src/train.ts
+++ src/train.ts
@@ -8,7 +8,17 @@
 
 // H-TASK
 
-function positiveNumbers(arr: number[]): string[] {
-    return arr.filter(str => str > 0).map(String);
+// function positiveNumbers(arr: number[]): string[] {
+//     return arr.filter(str => str > 0).map(String);
+// }
+// console.log(positiveNumbers([-1,-2,-3,4,5,6,-7,8]));
+
+// I-TASK
+
+function majorityElement(arr: number[]): number {
+    return arr.sort((a, b) =>
+        arr.filter(n => n === b).length - arr.filter(n => n === a).length
+    )[0];
 }
-console.log(positiveNumbers([-1,-2,-3,4,5,6,-7,8]));
+
+console.log(majorityElement([7,2,7,6,5,1,2,9,8,6,3,6,5]));
